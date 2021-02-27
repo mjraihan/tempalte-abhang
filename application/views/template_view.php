9376b6df3e4d49ae5d39835334fad5c9c6312b1e
yhidd <yhiddymers@gmail.com>
2021-02-27 19:45:24 +0700

    aaa

--- application/views/template_view.php
+++ application/views/template_view.php
@@ -42,10 +42,10 @@
 			<div class="menu-bg-wrap">
 				<div class="site-navigation">
 					<div class="row g-0 align-items-center">
-						<div class="col-8 col-md-2">
-							<a href="<?= base_url() ?>" class="logo m-0 float-start">PT. Apak</span></a>
+						<div class="col-8 col-md-4">
+							<a href="<?= base_url() ?>" class="logo m-0 float-start">PT. PRIMA SISTEM TERPADU</span></a>
 						</div>
-						<div class="col-2 col-md-8 text-center">
+						<div class="col-2 col-md-6 text-center">
 							<ul class="js-clone-nav d-none d-lg-inline-block text-start site-menu mx-auto">
 								<li class="active"><a href="<?= base_url() ?>">Home</a></li>
 								<li class="has-children">
