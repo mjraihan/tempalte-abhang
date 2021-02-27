40f8cd4caf05c1ddd0ea09770f618d9790743a1b
mjraihan <mj.9509@students.amikom.ac.id>
2021-02-27 09:34:13 +0700

    add favicon

--- application/views/template_view.php
+++ application/views/template_view.php
@@ -19,6 +19,26 @@
 	<script src="<?= THEMES_SITE ?>/js/bootstrap.bundle.min.js"></script>
 	<script src="<?= THEMES_SITE ?>/js/jquery-2.2.4.min.js"></script>
 	<title>PT Apak Sahajatra - <?= $title; ?></title>
+
+	<!-- Favicon -->
+    <link rel="apple-touch-icon" sizes="57x57" href="<?= THEMES_SITE ?>/images/favicon/apple-icon-57x57.png">
+	<link rel="apple-touch-icon" sizes="60x60" href="<?= THEMES_SITE ?>/images/favicon/apple-icon-60x60.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="<?= THEMES_SITE ?>/images/favicon/apple-icon-72x72.png">
+	<link rel="apple-touch-icon" sizes="76x76" href="<?= THEMES_SITE ?>/images/favicon/apple-icon-76x76.png">
+	<link rel="apple-touch-icon" sizes="114x114" href="<?= THEMES_SITE ?>/images/favicon/apple-icon-114x114.png">
+	<link rel="apple-touch-icon" sizes="120x120" href="<?= THEMES_SITE ?>/images/favicon/apple-icon-120x120.png">
+	<link rel="apple-touch-icon" sizes="144x144" href="<?= THEMES_SITE ?>/images/favicon/apple-icon-144x144.png">
+	<link rel="apple-touch-icon" sizes="152x152" href="<?= THEMES_SITE ?>/images/favicon/apple-icon-152x152.png">
+	<link rel="apple-touch-icon" sizes="180x180" href="<?= THEMES_SITE ?>/images/favicon/apple-icon-180x180.png">
+	<link rel="icon" type="image/png" sizes="192x192"  href="<?= THEMES_SITE ?>/images/favicon/android-icon-192x192.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="<?= THEMES_SITE ?>/images/favicon/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="96x96" href="<?= THEMES_SITE ?>/images/favicon/favicon-96x96.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="<?= THEMES_SITE ?>/images/favicon/favicon-16x16.png">
+	<link rel="manifest" href="<?= THEMES_SITE ?>/images/favicon/manifest.json">
+	<meta name="msapplication-TileColor" content="#ffffff">
+	<meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
+	<meta name="theme-color" content="#ffffff">
+
 </head>
 <body>
 	<div class="site-mobile-menu site-navbar-target">
